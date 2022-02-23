a042748e87b1335a5cb16cdc34e9944c5f1180fe
Muhammad <muhammadmnnm1@gmail.com>
2022-02-24 02:33:26 +0500

    Initial Commit

---
+++ HowAreYou/HowAreYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowAreYouApp.swift
+//  HowAreYou
+//
+//  Created by Muhammad Islamov on 24/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HowAreYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
